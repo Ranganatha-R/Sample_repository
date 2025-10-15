0cc8cf1a965e1c09ac356cd8259d9506b7681338
ppsr4362 <111430631+ppsr4362@users.noreply.github.com>
2025-10-15 11:54:22 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,6 +1,7 @@
 #this is nested for loop
-for i in range (1,10):
-        for j in range (1,10:
+for i in range (1,10): #loop 1
+        for j in range (1,10): #loop 2
                print(i*j, end= " ")
-print()
+        print()
 
+# comments are added
